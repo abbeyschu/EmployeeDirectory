d9fa6a7856604afe34255abdb4bcdacdfc336c9e
Abbey Schueller <schuellerabbey@gmail.com>
2021-06-15 21:43:09 -0700

    adding components

---
+++ employeedirectory/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Employee Directory</h1>
+      <div>Filter 1</div>
+    </header>
+  );
+}
+
+export default Header;
