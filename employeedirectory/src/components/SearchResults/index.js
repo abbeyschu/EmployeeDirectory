d28dd7ebde2f182975904cd7a13cf67b90a63454
Abbey Schueller <schuellerabbey@gmail.com>
2021-06-19 14:48:04 -0700

    updated css

--- employeedirectory/src/components/SearchResults/index.js
+++ employeedirectory/src/components/SearchResults/index.js
@@ -3,17 +3,19 @@ import "./style.css";
 
 function SearchResults(props) {
   return (
-    <ul className="list-group search-results">
+    <div className="employeeResults">
       {props.employees.map(employee => (
-        <li key={employee} className="list-group-item">
-          <img alt="Employee" src={employee.picture} className="img-fluid" />
+        <div key={employee} className = "oneEmployee">
+          <img alt="Employee" src={employee.picture.medium} className="employeeImage" />
+          <div className="employeeInfo">
           <h2>{employee.name.first} {employee.name.last}</h2>
-          <div>{employee.location.city} {employee.location.state}</div>
+          <div>{employee.location.city}, {employee.location.state}</div>
           <div>{employee.email}</div>
           <div>{employee.phone}</div>
-        </li>
+          </div>
+        </div>
       ))}
-    </ul>
+    </div>
   );
 }
 
