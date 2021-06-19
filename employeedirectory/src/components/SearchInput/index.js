c535238a237783df10377ce806265bcea88b74a9
Abbey Schueller <schuellerabbey@gmail.com>
2021-06-19 13:05:25 -0700

    updated package

--- employeedirectory/src/components/SearchInput/index.js
+++ employeedirectory/src/components/SearchInput/index.js
@@ -3,7 +3,12 @@ import "./style.css";
 
 function SearchInput(props) {
   return (
+    <div>
     <input></input>
+    <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+    Search
+  </button>
+  </div>
   );
 }
 
